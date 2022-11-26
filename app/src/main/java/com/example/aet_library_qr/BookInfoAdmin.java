ee8b2596f07d0a8440beede02fe39701a0cdec02
cskiller24 <ccadayong2020@plm.edu.ph>
2022-11-26 17:11:32 +0800

    Added book info message

--- app/src/main/java/com/example/aet_library_qr/BookInfoAdmin.java
+++ app/src/main/java/com/example/aet_library_qr/BookInfoAdmin.java
@@ -2,7 +2,9 @@ package com.example.aet_library_qr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BookInfoAdmin extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class BookInfoAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info_admin);
+
+        /**
+         * Pag kukuha ng book information need dito ng key
+         * Example book key: -NHiHLPWnjfS9csUJOSM (from firebase)
+         *
+         * Bale gagamit ng getIntents() para makuha yung key
+         * if ever na walang key, automatic redirect sa admin home page with message
+         */
+        Intent intent = getIntent();
+        Toast.makeText(BookInfoAdmin.this, "" + intent.getStringExtra("key"), Toast.LENGTH_SHORT).show();
     }
 }
